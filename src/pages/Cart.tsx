ab10725f010e4a6f70c7cd2fcfa7df658a5d7636
Kholmurodov <suhrobkholmurodov1@gmail.com>
2025-01-30 17:56:35 +0500

    minor changes

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import { Helmet } from 'react-helmet'
 import { CartItem } from '../redux/cart/types'
+import { Minus, Plus, Trash2 } from 'lucide-react'
 
 export const Cart = () => {
   const [cartItems, setCartItems] = useState([])
@@ -28,34 +29,48 @@ export const Cart = () => {
               key={item.id}
               className='flex items-center gap-4 p-4 sm:p-2 border-b border-gray-300'
             >
-              <div className='w-20 sm:w-[60px] sm:h-[60px] h-20'>
+              <div className='w-[250px] h-[250px]'>
                 <img
-                  className='w-full h-full object-cover rounded-md'
+                  className='object-cover w-full h-full rounded-md'
                   src={item.imageUrl}
                   alt={item.name}
                 />
               </div>
-              <div className='flex-1 dark:text-mainTextColor'>
-                <h3 className='text-lg sm:text-[13px] font-semibold'>
-                  {item.name}
-                </h3>
-                <p className='text-sm sm:text-[12px] text-gray-500'>
-                  {categoryNames[item.category]}
-                </p>
-                <p className='text-sm sm:text-[12px] text-gray-500'>
-                  Price: {item.price}$
-                </p>
-                <p className='text-sm sm:text-[12px] text-gray-500'>
-                  Count: {item.count}
-                </p>
-                <p>
-                  Option 1:
-                  {item.options.first.join(', ')}
-                </p>
-                <p>
-                  Option 2:
-                  {item.options.second.join(', ')}
-                </p>
+              <div className='grid grid-cols-1'>
+                <div>
+                  <p className='text-[21px] font-[700] text-gray-500'>
+                    {item.price}$.
+                  </p>
+                  <h3 className='text-lg sm:text-[13px] font-semibold'>
+                    {item.name}
+                  </h3>
+                  <p className='text-sm sm:text-[12px] text-gray-500'>
+                    {categoryNames[item.category]}
+                  </p>
+                  <div className='flex items-center gap-[20px]'>
+                    <p>Опции:</p>
+                    <p className='px-3 py-[2px] text-white rounded-full bg-gray-600'>
+                      {item.options.first}
+                    </p>
+                    <p className='px-3 py-[2px] text-white rounded-full bg-gray-600'>
+                      {item.options.second}
+                    </p>
+                  </div>
+                </div>
+                <div className='flex mt-2 items-center gap-[10px] justify-between'>
+                  <div className='flex gap-2 items-center'>
+                    <button className='p-2 bg-gray-200 rounded-full hover:bg-gray-300'>
+                      <Minus />
+                    </button>
+                    <p className='text-xl text-gray-700'>{item.count}</p>
+                    <button className='p-2 bg-gray-200 rounded-full hover:bg-gray-300'>
+                      <Plus />
+                    </button>
+                  </div>
+                  <button className='p-2 text-white rounded-md flex items-center space-x-2'>
+                    <Trash2 />
+                  </button>
+                </div>
               </div>
             </div>
           ))
